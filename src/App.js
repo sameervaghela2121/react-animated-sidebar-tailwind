5c5092a340fd390680e66f102633048a3ede92cb
sameervaghela2121 <sameervaghela21@gmail.com>
2022-08-16 13:37:28 +0530

    React Animated Sidebar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
